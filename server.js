756d4bc58f7baba0e9bc20b6926ba47e53c0f4a5
Damon Tian <damontian@live.com>
2017-08-10 17:52:47 -0400

    cors itunes issue 2

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var app = express();
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 // requires expressJS to use nodeJS to serve your website structure, so index.html will run from the root of folder and map to "/"
 app.use(express.static(__dirname + '/'));
 
